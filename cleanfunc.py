adding functions from notebook into cleanfunc - implemented some "fixes" for feature variance impacts

cleanfunc.py
@@ -12,7 +12,7 @@ from sklearn.ensemble import RandomForestRegressor
 import eli5
 import re
 import geocoder
-
+import operator
 
 nb_path = os.getcwd()
 nb_path
@@ -236,71 +236,150 @@ def feature_impact_plot(model, X_train, n_features, y_label):
     '''
     Takes a trained model and training dataset and synthesises the impacts of the top n features
     to show their relationship to the response vector (i.e. how a change in the feature changes
-    the prediction). Returns n plots showing the variance for min, max, median, 1Q and 3Q.
+    the prediction). Returns n plots showing the % variance for the median predictions made.
     
     INPUTS
-    model = trained supervised learning model
-    X_train = feature set the training was completed using
-    n_features = top n features you would like to plot
-    y_label - description of response variable for axis labelling
+    model = Trained model in sklearn with  variable ".feature_importances_". Trained supervised learning model.
+    X_train = Pandas Dataframe object. Feature set the training was completed using.
+    n_features = Int. Top n features you would like to plot.
+    y_label = String. Description of response variable for axis labelling.
+    
+    Currently the min, max, 1Q and 3Q are also stored in the variable, but plotting the % impacts 
+    for these makes the chart less intuitive so I recommend against plotting these all together.
+    There may be use in adding functionality allowing the selection of the quartile displayed?
     '''
     # Display the n most important features
     indices = np.argsort(model.feature_importances_)[::-1]
     columns = X_train.columns.values[indices[:n_features]]
     
-    ### THIS NEEDS UPDATING to integrate subplot mechanisms for line charts
-    #simulations = [[]] #deprecated
+    # Create a list object to capture the simulated prediction % variances to later turn into a DataFrame
     sim_var = [[]]
-    # For top 5 features
+    
     for col in columns:
-        base_pred = best_rf.predict(X.drop(drop_cols,axis=1))
+        base_pred = model.predict(X_train)
         #add percentiles of base predictions to a df for use in reporting
         base_percentiles = [np.percentile(base_pred, pc) for pc in range(0,101,25)]
-        #simulations.append(['base',col]+base_percentiles) #deprecated
 
         # Create new predictions based on tweaking the parameter
         # copy X, resetting values to align to the base information through different iterations
         df_copy = X_train.copy()
 
-        for val in np.arange(-X[col].std(), X[col].std(), X[col].std()/50):
-            df_copy[col] = X[col] + val
-            # add new predictions based on changed database
-            predictions = best_rf.predict(df_copy)
-            #add percentiles of these predictions to a df for use in reporting
+        for val in np.arange(-X_train[col].std(), X_train[col].std(), X_train[col].std()/50):
+            df_copy[col] = X_train[col] + val
+            # Add new predictions based on changed database
+            predictions = model.predict(df_copy)
+            
+            # Add percentiles of these predictions to a df for use in reporting
             percentiles = [np.percentile(predictions, pc) for pc in range(0,101,25)]
-            #simulations.append([val, col] + percentiles) #deprecated
-            # add variances between percentiles of these predictions and the base prediction to a df for use in reporting
+            
+            # Add variances between percentiles of these predictions and the base prediction to a df for use in reporting
             percentiles = list(map(operator.sub, percentiles, base_percentiles))
             percentiles = list(map(operator.truediv, percentiles, base_percentiles))
             sim_var.append([val, col] + percentiles)
 
-    # Plot a line chart based on the "describe()" function applied to this database
-    # Showing percentiles (min, 25th, 50th, 75th, max) over the series of values
+    # Create a dataframe based off the arrays created above
     df_predictions = pd.DataFrame(sim_var,columns = ['Value','Feature']+[0,25,50,75,100])
+    
+    # Create a subplot object based on the number of features
     num_cols = 2
-
-    fig, axs = plt.subplots(nrows = (int(n_features/num_cols) + int(n_features%num_cols)),
-                            ncols = num_cols, sharey = True, figsize=(15,15))
+    subplot_rows = int(n_features/num_cols) + int(n_features%num_cols)
+    fig, axs = plt.subplots(nrows = subplot_rows, ncols = num_cols, sharey = True, figsize=(15,5*subplot_rows))
 
     nlines = 1
 
+    # Plot the feature variance impacts
     for i in range(axs.shape[0]*axs.shape[1]):
         if i < len(columns):
-            axs[int(i/num_cols),int(i%num_cols)].plot(df_predictions[(df_predictions['Feature'] == columns[i]) & 
-                                    (df_predictions['Value'] != 'base')]['Value'],
-                     df_predictions[(df_predictions['Feature'] == columns[i]) & 
-                                    (df_predictions['Value'] != 'base')][50])
-            axs[int(i/num_cols),i%num_cols].set_title("\n".join(wrap(columns[i], int(100/num_cols))))
-            nlines = max(nlines,axs[int(i/num_cols),int(i%num_cols)].get_title().count('\n'))
+            # Cycle through each plot object in the axs array and plot the appropriate lines
+            ax_row = int(i/num_cols)
+            ax_column = int(i%num_cols)
+            
+            axs[ax_row, ax_column].plot(df_predictions[df_predictions['Feature'] == columns[i]]['Value'],
+                     df_predictions[df_predictions['Feature'] == columns[i]][50])
+            
+            axs[ax_row, ax_column].set_title("\n".join(wrap(columns[i], int(100/num_cols))))
+            
+            # Create spacing between charts if chart titles happen to be really long.
+            nlines = max(nlines, axs[ax_row, ax_column].get_title().count('\n'))
 
-            #format the y-axis as %
-            if int(i%num_cols) == 0:
-                vals = axs[int(i/num_cols),int(i%num_cols)].get_yticks()
-                axs[int(i/num_cols),int(i%num_cols)].set_yticklabels(['{:,.2%}'.format(x) for x in vals])
-                axs[int(i/num_cols),int(i%num_cols)].set_ylabel('% change to {}'.format(y_label))
-
+            # Format the y-axis as %
+            if ax_column == 0:
+                vals = axs[ax_row, ax_column].get_yticks()
+                axs[ax_row, ax_column].set_yticklabels(['{:,.2%}'.format(x) for x in vals])
+                axs[ax_row, ax_column].set_ylabel('% change to {}'.format(y_label))
+        
+        # If there is a "spare" plot, hide the axis so it simply shows ans an empty space
         else:
             axs[int(i/num_cols),int(i%num_cols)].axis('off')
-
+    
+    # Apply spacing between subplots in case of very big headers
+    fig.subplots_adjust(hspace=0.5*nlines)
+    
+    # Return the plot
     plt.tight_layout()    
     plt.show()
+
+def histo_plots(df, num_cols):
+    '''
+    Takes a dataframe object and plots each column as a histogram chart using the feature title as the 
+    chart title and the other columns as X-axis labels, within an n*(len/n) subplot frame
+    df: dataframe object with values from which you want a chart of each row
+    num_cols: the number of charts per output row
+    '''
+    rows = df.shape[0]
+    columns = df.shape[1]
+    plot_rows = (int(columns/num_cols) + ((columns%num_cols)!=0))
+    fig, axs = plt.subplots(nrows = plot_rows, 
+                            ncols = num_cols, sharey = False, figsize=(15,columns))
+    nlines = 1
+
+    for i in range(axs.shape[0]*axs.shape[1]):
+        if i < columns:
+            try:
+                df.iloc[:,i].dropna().plot.hist(ax = axs[int(i/num_cols),int(i%num_cols)])
+                axs[int(i/num_cols),i%num_cols].set_title("\n".join(wrap(df.columns[i], int(100/num_cols))))
+                nlines = max(nlines,axs[int(i/num_cols),int(i%num_cols)].get_title().count('\n'))
+            
+            except:
+                axs[int(i/num_cols),i%num_cols].set_title("\n".join(wrap(df.columns[i], 100/num_cols)))
+                axs[int(i/num_cols),i%num_cols].axis('off')
+        else:
+            axs[int(i/num_cols),int(i%num_cols)].axis('off')
+    
+    fig.subplots_adjust(hspace=0.5*nlines)
+    plt.tight_layout()    
+    plt.show()
+
+def feature_plot_h(model, X_train, n_features):
+    '''
+    Takes a trained model and training dataset and synthesises the impacts of the top n features.
+    Returns a horizontal bar plot showing the top n features and their corresponding feature importance.
+    
+    INPUTS
+    model = Trained model in sklearn with  variable ".feature_importances_". Trained supervised learning model.
+    X_train = Pandas Dataframe object. Feature set the training was completed using.
+    n_features = Int. Top n features you would like to plot.
+    
+    '''
+    # Display the n most important features
+    indices = np.argsort(model.feature_importances_)[::-1]
+    columns = X_train.columns.values[indices[:n_features]]
+    values = importances[indices][:n_features]
+    
+    columns = [ '\n'.join(wrap(c, 20)) for c in columns ]
+    
+    # Create the plot
+    fig = plt.figure(figsize = (9,n_features))
+    plt.title("Normalized Weights for {} Most Predictive Features".format(n_features), fontsize = 16)
+    plt.barh(np.arange(n_features), values, height = 0.6, align="center", color = '#00A000', 
+          label = "Feature Weight")
+    plt.barh(np.arange(n_features) - 0.3, np.cumsum(values), height = 0.2, align = "center", color = '#00A0A0', 
+          label = "Cumulative Feature Weight")
+    plt.yticks(np.arange(n_features), columns)
+    plt.xlabel("Weight", fontsize = 12)
+    
+    plt.legend(loc = 'upper right')
+    
+    plt.gca().invert_yaxis()
+    plt.tight_layout()
+    plt.show()  